63aed318a8287786d4ff1f92852753cd85187ec2
seu0310 <seu0310@konkuk.ac.kr>
2024-05-12 00:48:58 +0900

    make record screen layout

--- app/src/main/java/com/example/Healsenior/ui/ReplyApp.kt
+++ app/src/main/java/com/example/Healsenior/ui/ReplyApp.kt
@@ -17,14 +17,28 @@
 package com.example.Healsenior.ui
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.PermanentNavigationDrawer
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
@@ -33,7 +47,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastFirstOrNull
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -307,7 +330,7 @@ private fun ReplyNavHost(
             )
         }
         composable(ReplyRoute.Record) {
-            EmptyComingSoon()
+            RecordScreen()
         }
         composable(ReplyRoute.Community) {
             EmptyComingSoon()
@@ -315,5 +338,80 @@ private fun ReplyNavHost(
         composable(ReplyRoute.Mypage) {
             EmptyComingSoon()
         }
+    }
+}
+
+@Composable
+private fun RecordScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Gray)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        RecordScreenHeader()
+        Spacer(Modifier.height(30.dp))
+        WorkOutCalendar()
+        Spacer(Modifier.height(30.dp))
+        WorkOutRecord()
+    }
+}
+
+@Composable
+private fun RecordScreenHeader() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Gray)
+            .fillMaxWidth()
+            .height(50.dp)
+            .border(
+                width = 2.dp, // 너비 5dp
+                color = Color.Black, // 색상 파란색
+                shape = RectangleShape // 네모 모양
+            )
+    ) {
+
+    }
+}
+
+@Composable
+private fun WorkOutCalendar() {
+    Box(
+        modifier = Modifier
+            .width(300.dp)
+            .height(400.dp)
+            .border(
+                width = 2.dp, // 너비 5dp
+                color = Color.Blue, // 색상 파란색
+                shape = RoundedCornerShape(15.dp), // 네모 모양
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(15.dp)
+            ),
+    ) {
+
+    }
+}
+
+@Composable
+private fun WorkOutRecord() {
+    Text("23.11.14의 운동기록")
+    Box(
+        modifier = Modifier
+            .width(300.dp)
+            .height(150.dp)
+            .border(
+                width = 2.dp, // 너비 5dp
+                color = Color.Blue, // 색상 파란색
+                shape = RoundedCornerShape(15.dp), // 네모 모양
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(15.dp)
+            ),
+    ) {
+
     }
 }

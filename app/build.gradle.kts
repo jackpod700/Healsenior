f66b4174b650ae502a1b5d46e17ae6c6894d059a
jackpod7002 <jackpod7002@konkuk.ac.kr>
2024-06-02 18:57:53 +0900

    no message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {

0cfa8be6fd0a7af124c68c47b2e10557469da6d3
seu0310 <seu0310@konkuk.ac.kr>
2024-06-05 22:42:57 +0900

    implement playing video

--- app/src/main/java/com/example/Healsenior/workoutScreen/WorkOutProgressScreen.kt
+++ app/src/main/java/com/example/Healsenior/workoutScreen/WorkOutProgressScreen.kt
@@ -1,6 +1,5 @@
 package com.example.Healsenior.workoutScreen
 
-import android.media.browse.MediaBrowser
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,14 +12,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.PlayCircle
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,6 +34,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player.REPEAT_MODE_ALL
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.navigation.NavHostController
@@ -80,7 +79,7 @@ fun WorkOutProgressScreenContent(navController: NavHostController, workout: Muta
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         WorkOutScreenTimeBar(isStopped, hour, minute, second)
-        ShowWorkOutVideoContent(workout, workOutIdx.intValue)
+        ShowWorkOutVideoContent(workout, workOutIdx)
         ShowWorkOutDesciption(workout, workOutIdx.intValue)
         ShowWorkOutList(workout, workOutIdx.intValue)
         Row(
@@ -101,7 +100,7 @@ fun WorkOutProgressScreenContent(navController: NavHostController, workout: Muta
                         color = Color(0xFF5B9DFF),
                         shape = RoundedCornerShape(10.dp)
                     )
-                    .clickable() {
+                    .clickable {
                         isStopped.value = !isStopped.value
 
                         if (isStopped.value)
@@ -134,7 +133,7 @@ fun WorkOutProgressScreenContent(navController: NavHostController, workout: Muta
                         color = Color(0xFF5B9DFF),
                         shape = RoundedCornerShape(10.dp)
                     )
-                    .clickable() {
+                    .clickable {
                         if (workOutIdx.intValue + 1 < workout.size)
                             workOutIdx.intValue++
                         else if (workOutIdx.intValue + 1 == workout.size)
@@ -157,9 +156,8 @@ fun WorkOutProgressScreenContent(navController: NavHostController, workout: Muta
     }
 }
 
-//Todo 영상 링크 생기면 영상 재생 가능하게 하기
 @Composable
-fun ShowWorkOutVideoContent(workout: MutableList<Workout>, workOutIdx: Int) {
+fun ShowWorkOutVideoContent(workout: MutableList<Workout>, workOutIdx: MutableIntState) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -177,9 +175,46 @@ fun ShowWorkOutVideoContent(workout: MutableList<Workout>, workOutIdx: Int) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        val context = LocalContext.current
+        val exoplayer = remember {
+            ExoPlayer.Builder(context)
+                .build()
+                .also { exoPlayer ->
+                    exoPlayer.prepare()
+                    exoPlayer.playWhenReady = false
+                    exoPlayer.repeatMode = REPEAT_MODE_ALL
+                }
+        }
 
+        val mediaItem = MediaItem.fromUri("android.resource://com.example.Healsenior/${
+            if (workOutIdx.intValue == 0)
+                R.raw.air_running
+            else
+                R.raw.pull_up_out
+        
+        }")
+        exoplayer.setMediaItem(mediaItem)
+        exoplayer.prepare()
+        exoplayer.play()
 
+        AndroidView(
+            factory = {
+                PlayerView(it).apply {
+                    player = exoplayer
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
 
+        DisposableEffect(Unit) {
+            onDispose {
+                exoplayer.release()
+            }
+        }
     }
 }
 

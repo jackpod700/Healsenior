f0503a44bb50fbf1335bb63bf7a8484862dad10d
seu0310 <seu0310@konkuk.ac.kr>
2024-06-05 10:40:33 +0900

    commit

--- app/src/main/java/com/example/Healsenior/workoutScreen/WorkOutProgressScreen.kt
+++ app/src/main/java/com/example/Healsenior/workoutScreen/WorkOutProgressScreen.kt
@@ -1,5 +1,7 @@
 package com.example.Healsenior.workoutScreen
 
+import android.media.browse.MediaBrowser
+import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -25,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -33,7 +36,11 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import androidx.navigation.NavHostController
+import com.example.Healsenior.R
 import com.example.Healsenior._component.SmallTopBar
 import com.example.Healsenior.data.Workout
 import com.example.Healsenior.workoutScreen.workoutComponent.WorkOutScreenTimeBar
@@ -170,15 +177,9 @@ fun ShowWorkOutVideoContent(workout: MutableList<Workout>, workOutIdx: Int) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Icon(
-            Icons.Outlined.PlayCircle,
-            contentDescription = "",
-            tint = Color.Black,
-            modifier = Modifier
-                .width(100.dp)
-                .height(100.dp)
-        )
-        //영상 재생
+
+
+
     }
 }
 
